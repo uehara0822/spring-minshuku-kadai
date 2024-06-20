21ac264b5b79ccbd7fb8350c6eed1a08de0e6de5
uehara0822 <166109323+uehara0822@users.noreply.github.com>
2024-06-20 11:29:58 +0900

    Delete kadai_tutorial_output/src/main directory

--- kadai_tutorial_output/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51PSvNDP8PEmlUBaKZHF3wB8nEHcPcetUKELTPZdjJ5xQlnlEZ8APWr4vhjgfOeGBwKTrNN3Ve8GEmDRPqpBDmco600KrpD6bcx');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
